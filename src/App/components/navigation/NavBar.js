f155de26cbd84ef7e2ff10678135a993599dadfc
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-04-28 10:07:00 -0500

    setup files

---
+++ src/App/components/navigation/NavBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavbarText
+} from 'reactstrap';
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Home</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link className="nav-link" to="/weather">Weather</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/jokes">Jokes</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/lyrics">Lyrics</Link>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
